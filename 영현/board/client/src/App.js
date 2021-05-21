0228a0a779ab691229c2a36f3f3bf9dcbb3ea1fc
YoungHyun197 <cyh197@naver.com>
2021-05-21 14:41:35 +0900

    Add Get Customers REST API Modul

--- 영현/board/client/src/App.js
+++ 영현/board/client/src/App.js
@@ -19,32 +19,27 @@ const styles ={
     }
   };
 
-const customers = [{
-  'id' : 1,
-  'name' : '홍길동',
-  'image' : 'https://placeimg.com/64/64/1', // 랜덤으로 이미지 64*64로보여줌
-  'birthday': '970712',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id' : 2,
-  'name' : '김철수',
-  'image' : 'https://placeimg.com/64/64/2', 
-  'birthday': '991003',
-  'gender': '남자',
-  'job': '군인'
-},
-{
-  'id' : 3,
-  'name' : '이영희',
-  'image' : 'https://placeimg.com/64/64/3', 
-  'birthday': '010322',
-  'gender': '여자',
-  'job': '미용사'
-}]
+
+
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res=> this.setState({customers: res}))
+    .catch(err=>console.log(err));
+  }
+  
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes} = this.props;
     return(
@@ -61,7 +56,9 @@ class App extends Component {
              </TableRow>
            </TableHead>
            <TableBody>
-             { customers.map(c => {return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+             {this.state.customers ? this.state.customers.map(c => {
+               return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job}/>);
+              }) : ""}
            </TableBody>
           </Table>
       </Paper>
--- 영현/board/server.js
+++ 영현/board/server.js
@@ -6,8 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res)=> {
-    res.send({message:"Hello Express!"});
+app.get('/api/customers', (req, res)=> {
+    res.send([{
+        'id' : 1,
+        'name' : '홍길동',
+        'image' : 'https://placeimg.com/64/64/1', // 랜덤으로 이미지 64*64로보여줌
+        'birthday': '970712',
+        'gender': '남자',
+        'job': '대학생'
+      },
+      {
+        'id' : 2,
+        'name' : '김철수',
+        'image' : 'https://placeimg.com/64/64/2', 
+        'birthday': '991003',
+        'gender': '남자',
+        'job': '군인'
+      },
+      {
+        'id' : 3,
+        'name' : '이영희',
+        'image' : 'https://placeimg.com/64/64/3', 
+        'birthday': '010322',
+        'gender': '여자',
+        'job': '미용사'
+      }])
 });
-
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
