a526b7a315a62b3ee6d009f233cbb08761b30824
SortingHyeok <balkans0502@hanmail.net>
2021-07-03 20:01:35 +0900

    message

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <MessengerCustomerChat
+        pageId="100026604757377"
+        appId="3052504145007305"
+     />,
+    </div>
+  );
+}
+
+export default App;
