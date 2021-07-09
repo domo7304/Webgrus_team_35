af743bacb9f98b522f70d15605ccdf35134b829f
rxb8k <82093280+rxb8k@users.noreply.github.com>
2021-07-09 12:51:22 +0900

    프론트 병합 이전
    210709 프론트 병합 이전 / 회원가입 로그인 ID찾기 PW찾기

--- 진서/기존/src/App.js
+++ 진서/기존/src/App.js
@@ -1,29 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import SignPage from "./pages/signup";
-import LoginPage from "./pages/login";
-import findID from "./pages/findid";
-import findPW from "./pages/findpw";
-import Header from "./component/header";
-
-function App() {
-    return (
-        <>
-            <Header />
-            <Router>
-                <Route path="/signup" exact component={SignPage} />
-                <Route path="/login" exact component={LoginPage} />
-                <Route path="/findid" exact component={findID} />
-                <Route path="/findpw" exact component={findPW} />
-            </Router>
-        </>
-    );
-}
-
-export default App;
-
-/*
-
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import "./App.css";
@@ -40,7 +14,7 @@ function App() {
         setInputPw(e.target.value);
     };
 
-   // login 버튼 클릭 이벤트
+  /*  // login 버튼 클릭 이벤트
     const onClickLogin = () => {
         console.log("click login");
     };
@@ -55,7 +29,7 @@ function App() {
         },
         // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
         []
-    );
+    );*/
 
     return (
         //html
@@ -83,4 +57,4 @@ function App() {
     );
 }
 
-export default App;*/
+export default App;
