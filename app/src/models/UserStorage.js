4aad3f05f75f37e91ea49e4ed70f530379211282
DOWON LEE <domo7304@naver.com>
2021-07-10 15:17:01 +0900

    UserStorage 구현, 원하는 로그인 인증을 위한 get함수 작성

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,10 +1,27 @@
 "use strict";
 
 class UserStorage {
-  static users = {
+  // 클래스 안의 변수는 const 와 같은 표현이 필요x
+  // 클래스에서 직접 users에 접근하기 위해 static 선언
+  // # 의 역할이 private 과 같은 건가..?
+  static #users = {
     id : ["1111", "2222", "3333"],
     password : ["1111", "2222", "3333"],
+    name : ["user1", "user2", "user3"],
   };
+  /// 매개변수를 ...으로 해주면 입력받은 매개변수를 배열 형으로 받는다
+  static getUsers(...fields){
+    const users = this.#users;
+    // return users;
+    // 위와 같이 하면 user는 전부 받을 수 있지만 원하는 원소만 골라내는 것은 불가
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)){
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
+// app.js 파일 안의 app.use 코드를 통해서 routes/home/index.js 로 진입
+// router.get(), router.post() 에 각 해당하는 경로에 맞는 콜백함수를 실행하게 됨
+// 라우팅 역할
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.post("/login", ctrl.process.login);
