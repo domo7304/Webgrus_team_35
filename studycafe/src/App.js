c4626debf7b1e677380cfddca607d90c1748b2f5
송진서 <12183887@inha.edu>
2021-07-27 13:28:36 +0900

    2021. 07. 27 병합 전 push
    로그인 폼 함수 추가, 백이랑 연결 후 동작 확인 필요

--- studycafe/src/App.js
+++ studycafe/src/App.js
@@ -6,28 +6,27 @@ import SignPage from "./pages/signup";
 import LoginPage from "./pages/login";
 import findID from "./pages/findid";
 import findPW from "./pages/findpw";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-
+import MessengerCustomerChat from "react-messenger-customer-chat";
+import customer from "./components/Customer/customer";
 
 function App() {
     return (
         <div>
-        <Router>
-            
-            <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="/signup" exact component={SignPage} />
-                <Route path="/login" exact component={LoginPage} />
-                <Route path="/findid" exact component={findID} />
-                <Route path="/findpw" exact component={findPW} />
-            </Switch>
-        </Router>
-        
-        <MessengerCustomerChat
-        pageId="111618531178432"
-        appId="3052504145007305"
-        />
-     </div>
+            <Router>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/signup" exact component={SignPage} />
+                    <Route path="/login" exact component={LoginPage} />
+                    <Route path="/findid" exact component={findID} />
+                    <Route path="/findpw" exact component={findPW} />
+                </Switch>
+            </Router>
+
+            <MessengerCustomerChat
+                pageId="111618531178432"
+                appId="3052504145007305"
+            />
+        </div>
     );
 }
 
--- studycafe/src/pages/login.jsx
+++ studycafe/src/pages/login.jsx
@@ -38,77 +38,99 @@ const useStyles = makeStyles((theme) => ({
 export default function LogIn() {
     const classes = useStyles();
 
+    const [inputId, setInputId] = useState("");
+    const [inputPw, setInputPw] = useState("");
+
+    // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
+    const handleInputId = (e) => {
+        setInputId(e.target.value);
+    };
+
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value);
+    };
+
+    // login 버튼 클릭 이벤트
+    const onClickLogin = () => {
+        console.log("click login");
+    };
+
     return (
         <Container1>
-        
-        <Icon to="/">Study Joa</Icon>
-        <Container component="main" maxWidth="xs" backgroundColor='red'>
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    로그인
-                </Typography>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="ID"
-                        label="ID"
-                        name="ID"
-                        autoComplete="ID"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="자동로그인"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
+            <Icon to="/">Study Joa</Icon>
+            <Container component="main" maxWidth="xs" backgroundColor="red">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
                         로그인
-                    </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="/findid" variant="body2">
-                                ID 찾기
-                            </Link>
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="ID"
+                            label="ID"
+                            name="ID"
+                            autoComplete="ID"
+                            value={inputId}
+                            onChange={handleInputId}
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            value={inputPw}
+                            onChange={handleInputPw}
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox value="remember" color="primary" />
+                            }
+                            label="자동로그인"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={onClickLogin}
+                        >
+                            로그인
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="/findid" variant="body2">
+                                    ID 찾기
+                                </Link>
+                            </Grid>
+                            <Grid item xs>
+                                <Link href="/findpw" variant="body2">
+                                    PW 찾기
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    {"회원가입"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item xs>
-                            <Link href="/findpw" variant="body2">
-                                PW 찾기
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="/signup" variant="body2">
-                                {"회원가입"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={8}></Box>
-        </Container>
-       
+                    </form>
+                </div>
+                <Box mt={8}></Box>
+            </Container>
         </Container1>
     );
 }
