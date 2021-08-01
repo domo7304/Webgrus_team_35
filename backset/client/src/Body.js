3dc03e11bc631fe4ea3d60681d1bfd25693f9cb2
jhLee <sortinghyeok@gmail.com>
2021-08-02 01:16:53 +0900

    board url 수정 + admin template 추가

--- backset/client/src/Body.js
+++ backset/client/src/Body.js
@@ -14,11 +14,11 @@ class Body extends Component {
     function getResultForm() {
       // console.log($.cookie("login_id"));
       if ($.cookie("login_id")) {//쿠키 존재시 BoardForm, 즉 게시판으로 바로 넘어간다. component에 들어있는 값을 호출한다고 보면 되는 것.
-        resultForm = <Route exact path="/" component={BoardForm}></Route>;
+        resultForm = <Route exact path="/board" component={BoardForm}></Route>;
         //main url에 있는 다른 페이지들이 딸려나오지 않도록 "/"처리해준다.
         return resultForm;
       } else { //쿠키가 존재하지 않으면, LoginForm으로 가게 된다. react이므로 각 js파일에 스타일이 적용되어있어 별도의 css파일이 존재하지 않음을 기억하자.
-        resultForm = <Route exact path="/" component={LoginForm}></Route>;
+        resultForm = <Route exact path="/board" component={LoginForm}></Route>;
         return resultForm;
       }
     }
