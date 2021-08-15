944af787fc13a09a5d9a4ea4bffdd76051f1d2f1
SSiik <sik_5571@naver.com>
2021-08-15 15:24:31 +0900

    실시간 좌석 확인 개선
    실시간 좌석 확인 개선

--- backset/server/routes/bookingRouter.js
+++ backset/server/routes/bookingRouter.js
@@ -5,9 +5,10 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 const connectionURL = "mongodb+srv://root:1234@logindb.xsreo.mongodb.net/webDB?retryWrites=true&w=majority";
 const databaseName = "webDB";
+var timer =[];
 
 //원하는시간을 투입한다. 일단 여기서 디폴트는 0으로설정.
-function getCurrentDate(hour){ 
+function getCurrentDate(hour=0){ 
   var date = new Date();
   var year = date.getFullYear(); 
   var month = date.getMonth();
@@ -18,6 +19,7 @@ function getCurrentDate(hour){
   var milliseconds = date.getMilliseconds();
   return new Date(Date.UTC(year, month, today, hours, minutes, seconds, milliseconds));
 }
+
 
 /* 함수사용할때 저렇게 지금 내버전에서는 인자로 넣어서 콜백함수를 사용하자. */
 router.get('/showSeats', (req, res) => {
@@ -53,18 +55,28 @@ router.post('/booking', (req, res) => {
       }
       console.log("Connected correctly.");
       const db = client.db(databaseName);   // db가 없으면 생성. 있으면 조회
+      var addTime=parseInt(req.body.addTime)
+      var seatNoNum = parseInt(req.body.seatNoNum)
      db.collection("seats").replaceOne(
-       {seatNo:req.body.seatNo}, //seatNo를 String형으로 바꾸어서 진행햇더니 성공햇다.
+       {seatNo:req.body.seatNo}, 
        {
           seatNo:req.body.seatNo, 
-          seatNoNum : req.body.seatNoNum,
+          seatNoNum : seatNoNum,
           isAvailable:false,
           userId:req.body.userId,
-          endTime:getCurrentDate(req.body.addTime)
+          endTime:getCurrentDate(addTime)
        }
      )
+     var ele = {  //추가된 부분 
+       seatNo:req.body.seatNo,
+       endTime:getCurrentDate(addTime)
+     }
+     timer.push(ele);
      return res.json({success:true})
 })
 })
 
-module.exports = router;
+module.exports = {
+  router:router,
+  timer:timer
+}
