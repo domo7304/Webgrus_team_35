c537130deda274eac3b4f3eabdd119622198a652
DOWON LEE <domo7304@naver.com>
2021-08-18 18:16:29 +0900

    id찾기, pw변경, 사용자 인증 모두 완료

--- backset/server/middleware/auth.js
+++ backset/server/middleware/auth.js
@@ -1,48 +1,68 @@
-const { User } = require("../models/User");
+const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
   // 사용자의 요청이 인증을 필요로 할 때마다 client <-> server 간 사용자 인증을 처리
   // client 쿠키에서 토큰을 가져와 verify
-  const token = req.cookies.jwt;
-
-  if (token){
-    jwt.verify(token, 'secretToken', (err, decodedToken) => {
-      if (err){
-        console.log(err.message);
-        // 로그인페이지로 이동시키기
-      } else {
-        console.log(decodedToken);
-        next();
-      }
-    });
-  } else {
-    // 로그인페이지로 이동시키기
-  }
-}
-
-const checkUser = (req, res, next) => {
+  
   const token = req.cookies.jwt;
 
   if (token){
     jwt.verify(token, 'secretToken', async (err, decodedToken) => {
       if (err){
-        console.log(err.message);
         req.user = null;
         next();
       } else {
-        console.log(decodedToken);
         let user = await User.findById(decodedToken.id);
         req.user = user;
         next();
       }
     });
   } else {
+    console.log(`사용자 정보가 존재하지 않습니다`);
     res.user = null;
     next();
   }
+  
+  // const token = req.cookies.jwt;
+
+  // if (token){
+  //   jwt.verify(token, 'secretToken', (err, decodedToken) => {
+  //     if (err){
+  //       console.log(err.message);
+  //       next();
+  //     } else {
+  //       console.log(decodedToken);
+  //       next();
+  //     }
+  //   });
+  // } else {
+  //   next();
+  // }
 }
+
+// const checkUser = (req, res, next) => {
+//   const token = req.cookies.jwt;
+
+//   if (token){
+//     jwt.verify(token, 'secretToken', async (err, decodedToken) => {
+//       if (err){
+//         req.user = null;
+//         next();
+//       } else {
+//         let user = await User.findById(decodedToken.id);
+//         req.user = user;
+//         next();
+//       }
+//     });
+//   } else {
+//     console.log(`사용자 정보가 존재하지 않습니다`);
+//     res.user = null;
+//     next();
+//   }
+// }
 
 module.exports = { 
   auth,
-  checkUser
+  //checkUser
 };
